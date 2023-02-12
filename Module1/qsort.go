ff42fffb1d11830f0630696034c4e486717715f4
Александр Никитин <121054417+Olegsandrik@users.noreply.github.com>
2023-02-12 16:17:07 +0300

    Create qsort.go

---
+++ Module1/qsort.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt" //это пакет
+
+var arr []int
+
+func swap(i int, j int) {
+	arr[i], arr[j] = arr[j], arr[i]
+}
+func less(i int, j int) bool {
+	if arr[i] >= arr[j] {
+		return false
+	}
+	return true
+}
+func Partition(swap func(i, j int), less func(i, j int) bool, low int, high int) int {
+	i := low
+	j := high
+	for j < high {
+		if less(arr[j], arr[high]) {
+			swap(arr[i], arr[j])
+			i++
+		}
+		j++
+	}
+	swap(arr[i], arr[high])
+	return i
+}
+func QuickSortRec(less func(i, j int) bool, swap func(i, j int), low int, high int) {
+	if less(low, high) {
+		q := Partition(swap, less, low, high)
+		QuickSortRec(less, swap, low, q-1)
+		QuickSortRec(less, swap, q+1, high)
+	}
+}
+func qsort(n int, less func(i, j int) bool, swap func(i, j int)) {
+	QuickSortRec(less, swap, 0, n-1)
+}
+
+func main() {
+	//fmt.Println("This is IT!")
+	var n int
+	fmt.Scan(&n)
+	arr = make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&arr[i])
+	}
+	qsort(n, less, swap)
+	fmt.Println(arr)
+}
